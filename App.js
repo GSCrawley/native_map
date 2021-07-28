f47175d4ccd828eee6bd485064fa7a4833f4411c
Gideon Crawley <gscrawley@gmail.com>
2021-07-28 15:26:27 -0700

    created map component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// COMPONENTS
+
+import Map from "./components/Map";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-       
+        <Map />       
     </View>
   );
 }
---
+++ components/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Map = props => {
+return (
+    <View style={styles.container}>
+      <Text>My Map</Text>
+       
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Map;
